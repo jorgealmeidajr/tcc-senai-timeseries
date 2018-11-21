f479887b7b2c0f0ba0989d46583ca0084e672142
Jorge Almeida Junior <jorgealmeidajunior@gmail.com>
2018-11-21 16:21:48 -0200

    Mlp aplicado na serie TS03 finalizado.

---
+++ src/ts03-mlp-gridsearch.py
@@ -0,0 +1,47 @@
+
+import warnings
+import pandas as pd
+import numpy as np
+
+import data
+import constants
+import mlp
+
+
+
+OUTPUT_M: str = 'output\\ts03\\ts03-m-mlp.csv'
+OUTPUT_M2: str = 'output\\ts03\\ts03-m2-mlp.csv'
+
+# carrego a serie temporal mensal com duas amostras
+df_monthly2 = data.load_timeseries03_monthly2()
+
+# carrego a serie temporal mensal
+df_monthly = data.load_timeseries03_monthly()
+
+
+def main():
+  print('\nMLP: Busca pela melhor REDE NEURAL - Serie Temporal TS03')
+  #grid_search_ts_monthly2()
+  #grid_search_ts_monthly()
+
+
+def grid_search_ts_monthly2():
+  print(' > MLP: Grid Search na serie MENSAL com duas amostras')
+  train, test = mlp.split_to_train_test(df_monthly2, porcentagem=constants.PORCENTAGEM, debug=True)
+
+  mlp.evaluate_mlp_models(train, test, OUTPUT_M2)
+  print()
+
+
+def grid_search_ts_monthly():
+  print('> MLP: Grid Search na serie MENSAL')
+  train, test = mlp.split_to_train_test(df_monthly, porcentagem=constants.PORCENTAGEM, debug=True)
+
+  mlp.evaluate_mlp_models(train, test, OUTPUT_M)
+  print()
+
+
+
+if __name__ == '__main__':
+  warnings.filterwarnings("ignore")
+  main()
